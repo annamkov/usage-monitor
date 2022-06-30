e6a5456c740f003783e4656236e2b71d63a7cff3
Anna Messejnikov <anna.messejnikov@gmail.com>
2022-06-30 00:56:18 -0500

    implemented a simple monitor

---
+++ monitor.py
@@ -0,0 +1,16 @@
+import psutil
+import time
+
+#\r (carriage return) resets the cursor position to the beginning of the line,
+# which makes future print statement periodically overwrite the previous contents.
+# end = "" sets the last character of print to be an empty string instead of the default \n.
+# this keeps the output occuring on the same line
+
+def show_usage(cpu_usage, bars=50):
+    cpu_percent = (cpu_usage / 100.00)
+    cpu_bar = '█' * int(cpu_percent * bars) + '-' * (bars - int(cpu_percent * bars))
+    print(f"\rCPU Usage: |{cpu_bar}| |{cpu_usage}%|", end="")
+
+while(True):
+    show_usage(psutil.cpu_percent())
+    time.sleep(0.50)
